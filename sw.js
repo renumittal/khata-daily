5421daf2b5c64e192d36faade1090afad12e1e29
Renu Mittal <renugoyal@gmail.com>
2026-09-08 23:08:52 +0530

    Bump service worker cache version so the Credit/Debit button update actually reaches browsers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'khata-daily-v2';
+const CACHE = 'khata-daily-v3';
 const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
